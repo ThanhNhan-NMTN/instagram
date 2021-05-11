5ef5dfff4796b2a0961bec539de0a899f204b7fc
ThanhNhan-NMTN <nguyennhan24102002@gmail.com>
2021-05-11 23:26:11 +0700

    first commit

---
+++ instagram/src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+function App() {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  // eslint-disable-next-line react/jsx-filename-extension
+  return <p>Hello</p>;
+}
+
+export default App;
